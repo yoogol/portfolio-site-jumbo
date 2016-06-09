af67b778ffbdee77c7a765c8a216f63c6adbb649
Yulia Shea <yulia.shea@gmail.com>
2016-06-09 01:10:25 -0400

    sever file added

---
+++ server.js
@@ -0,0 +1,10 @@
+
+var express = require('express'),
+    PORT    = process.env.PORT || 3000,
+    app     = express();
+
+app.use(express.static('./'));
+
+app.listen(PORT, function () {
+  console.log('server is listening on port ' + PORT + "!");
+});
